4935075bda764ee2cc88c3ebb846b24bbf12acb2
hh <dbn0224@gmail.com>
2021-08-02 15:54:39 +0900

    재구성 완료

---
+++ runningbuddy/app/src/main/java/com/earlybird/runningbuddy/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.earlybird.runningbuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.earlybird.runningbuddy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
