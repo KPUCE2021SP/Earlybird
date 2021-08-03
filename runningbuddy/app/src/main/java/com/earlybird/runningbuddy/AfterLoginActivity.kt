f349b0e10dfe0bc63181bb7a92fba87fe7cacae4
onkiCh <54262393+onkiCh@users.noreply.github.com>
2021-08-03 19:18:50 +0900

    맵버튼 추가버전
    맵버튼 추가

--- runningbuddy/app/src/main/java/com/earlybird/runningbuddy/AfterLoginActivity.kt
+++ runningbuddy/app/src/main/java/com/earlybird/runningbuddy/AfterLoginActivity.kt
@@ -27,8 +27,9 @@ class AfterLoginActivity : AppCompatActivity() {
             finish()
         }
         binding.buttonMap.setOnClickListener {
-            Firebase.auth.signOut()
-            finish()
+            startActivity(
+                Intent(this, MapActivity::class.java)
+            )
         }
     }
 
