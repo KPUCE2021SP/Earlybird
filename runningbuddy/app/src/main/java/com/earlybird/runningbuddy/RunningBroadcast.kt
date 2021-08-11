0a0ff0fe28c50d01ff0a6ee57137266ccca1b548
1ilkwon <dlfrnjs318@gmail.com>
2021-08-11 10:09:04 +0900

    브로드캐스트 클래스
    브로드캐스트 클래스 만들기

---
+++ runningbuddy/app/src/main/java/com/earlybird/runningbuddy/RunningBroadcast.kt
@@ -0,0 +1,27 @@
+package com.earlybird.runningbuddy
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.util.Log
+
+class RunningBroadcast : BroadcastReceiver() {
+    private val br = RunningBroadcast()
+    private val intentFilter = IntentFilter()
+
+//    fun onCreate(savedInstanceState: Bundle?){
+//        val filter = IntentFilter(Intent.ACTION_LOCALE_CHANGED)
+//        onReceiver(br, filter)
+//
+//    }
+
+
+    override fun onReceive(context: Context, intent: Intent) {
+        fun onReceive(context: Context?, intent: Intent?){
+            Log.d("브로드캐스트 슈루룽","Intent: $intent")
+        }
+
+    }
+}
